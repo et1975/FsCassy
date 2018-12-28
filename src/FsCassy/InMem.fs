d963e19546c3c824d37c61444d729f7e4f0070a1
Justin Sacks <justin.sacks@prolucid.ca>
2018-12-28 09:57:27 -0500

    Fixes #2, paket and sln

--- src/FsCassy/InMem.fs
+++ src/FsCassy/InMem.fs
@@ -3,18 +3,53 @@ module FsCassy.InMem
 open System
 open System.Linq
 open System.Linq.Expressions
-open System.Threading.Tasks
 
 /// Executes statements on a ResizeArray
 module Interpreter =
-    open System.Threading.Tasks
     open System.Collections.Generic
+    open System.Runtime.Serialization
+    
+    type Selection<'t> = 
+        | New of 't
+        | Existing of int*'t
+    module Selection =
+        let existing = function | Existing (i,v) -> Some (i,v) | New _ -> None
+        let filterExistingValue f = function | New _ -> false | Existing (_,v) -> f v
+        let filterExistingIndex f = function | New _ -> false | Existing (i,_) -> f i
+        let map f = function | Existing (i,v) -> Existing (i, f v) | New v -> New (f v)
+        let iterExisting f = function | Existing (i,v) -> f (i, v) | New v -> ()
+
+    let private mkDefault (x:Expression<Func<'t,bool>>) =
+        let destType = x.Parameters.[0].Type
+        let rec parseMemberValues : (Expression * Expression) -> (string * obj) list =
+            function
+            | (:? BinaryExpression as be1), (:? BinaryExpression as be2) ->
+                ((be1.Left, be1.Right) |> parseMemberValues) @ ((be2.Left, be2.Right) |> parseMemberValues)
+
+            | (:? MemberExpression as me), (:? ConstantExpression as ce)
+            | (:? ConstantExpression as ce), (:? MemberExpression as me) -> [ me.Member.Name, ce.Value ]
+
+            | (:? UnaryExpression as ue), (:? ConstantExpression as ce)
+            | (:? ConstantExpression as ce), (:? UnaryExpression as ue) -> (ue.Operand |> unbox<MemberExpression>, ce) |> parseMemberValues // [ (unbox<MemberExpression> ue.Operand).Member.Name, ce.Value ]
+
+            | e1, e2 -> failwith <| sprintf "Unsupported binary expression: %O %O" e1 e2
+
+        let queryValues = match x.Body with | :? BinaryExpression as be -> (be.Left, be.Right) |> parseMemberValues | e -> failwith <| sprintf "Unsupported root expression %O" e
+        let initValues =
+            FSharp.Reflection.FSharpType.GetRecordFields destType
+            |> Array.map (fun pi -> 
+                queryValues 
+                |> List.tryFind (fun (n, _) -> n = pi.Name)
+                |> Option.map snd 
+                |> Option.defaultValue (try FormatterServices.GetUninitializedObject pi.PropertyType with | _ -> null))
+
+        unbox<'t> <| FSharp.Reflection.FSharpValue.MakeRecord(destType, initValues)
 
     [<Struct>]
     type InterpretationState<'t> = 
         | Array of array:'t ResizeArray 
         | Query of query:Async<IEnumerable<'t>>
-        | Selection of selection:Async<IEnumerable<int*'t>>
+        | Selection of selection:Async<IEnumerable<Selection<'t>>>
         | Command of command:Async<unit>
         | Done
 
@@ -47,25 +82,28 @@ module Interpreter =
             | Clause(Where(x, mkNext)), Querylike q ->
                 let next = mkNext QueryStatement
                 let w = x.Compile().Invoke
-                async { let! q' = q in return q' |> Seq.mapi tuple |> Seq.filter (snd >> w) }
+                let mkSelection q' = 
+                    if q' |> Seq.exists w |> not then mkDefault x |> New |> Seq.singleton 
+                    else q' |> Seq.mapi tuple |> Seq.filter (snd >> w) |> Seq.map Existing
+                async { let! q' = q in return mkSelection q' }
                 |> (Selection >> tuple next >> recurse)
             
             | Clause(Where(x, mkNext)), Selection q ->
                 let next = mkNext QueryStatement
                 let w = x.Compile().Invoke
-                async { let! q' = q in return q' |> Seq.filter (snd >> w) }
+                async { let! q' = q in return q' |> Seq.filter (Selection.filterExistingValue w) }
                 |> (Selection >> tuple next >> recurse)
             
             | Clause(Select(x, mkNext)), Querylike q -> 
                 let next = mkNext QueryStatement
                 let sel = x.Compile().Invoke
-                async { let! q' = q in return q' |> Seq.mapi (fun i v -> i,sel v) }
+                async { let! q' = q in return q' |> Seq.mapi (fun i v -> Existing (i,sel v)) }
                 |> (Selection >> tuple next >> recurse)
             
             | Clause(Select(x, mkNext)), Selection q -> 
                 let next = mkNext QueryStatement
                 let sel = x.Compile().Invoke
-                async { let! q' = q in return q' |> Seq.map (fun (i,v) -> i,sel v) }
+                async { let! q' = q in return q' |> Seq.map (Selection.map sel) }
                 |> (Selection >> tuple next >> recurse)
             
             | Clause(UpdateIf(x, mkNext)), Selection q -> 
@@ -73,15 +111,17 @@ module Interpreter =
                 let iff = x.Compile().Invoke
                 async { 
                     let! q' = q
-                    let res = q' |> Seq.filter (fun (i,_) -> iff source.[i]) |> List.ofSeq
-                    res |> List.iter (fun (i,v) -> source.[i] <- v)
+                    let res = q' |> Seq.filter (Selection.filterExistingIndex (fun i -> iff source.[i])) |> List.ofSeq
+                    res |> List.iter (Selection.iterExisting (fun (i,v) -> source.[i] <- v))
                 } |> (Command >> tuple next >> recurse)
             
             | Clause(Update(mkNext)), Selection q -> 
                 let next = mkNext CommandStatement
                 async { 
                     let! q' = q
-                    q' |> List.ofSeq |> List.iter (fun (i,v) -> source.[i] <- v)
+                    q' 
+                    |> List.ofSeq 
+                    |> List.iter (function | Existing (i,v) -> source.[i] <- v | New v -> source.Add v)
                 } |> (Command >> tuple next >> recurse)
             
             | Clause(Delete(mkNext)), Querylike q -> 
@@ -96,7 +136,7 @@ module Interpreter =
                 let next = mkNext CommandStatement
                 async { 
                     let! q' = q
-                    let res = q' |> Seq.sortByDescending fst |> List.ofSeq
+                    let res = q' |> Seq.choose Selection.existing |> Seq.sortByDescending fst |> List.ofSeq
                     res |> List.iter (fst >> source.RemoveAt)
                 } |> (Command >> tuple next >> recurse)
             
@@ -119,7 +159,7 @@ module Interpreter =
             | Clause(Count(mkNext)), Selection q -> 
                 let next = async {
                                 let! q' = q
-                                return q' |> Seq.length |> int64 
+                                return q' |> Seq.choose Selection.existing |> Seq.length |> int64 
                            } |> mkNext
                 recurse (next,Done) 
             
@@ -130,7 +170,7 @@ module Interpreter =
             | Clause(Read(mkNext)), Selection q -> 
                 let next = async {
                                 let! q' = q
-                                return q' |> Seq.map snd
+                                return q' |> Seq.choose Selection.existing |> Seq.map snd
                             } |> mkNext
                 recurse (next,Done) 
             
@@ -144,7 +184,7 @@ module Interpreter =
             | Clause(Find(mkNext)), Selection q -> 
                 let next = async {
                                 let! q' = q 
-                                return q' |> Seq.map snd |> Seq.tryHead
+                                return q' |> Seq.choose Selection.existing |> Seq.map snd |> Seq.tryHead
                            } |> mkNext
                 recurse (next,Done) 
             
