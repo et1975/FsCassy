76472f82490f4617b3e8fc7cca007a5b6860706d
Justin Sacks <2sComplement@users.noreply.github.com>
2018-12-29 11:56:09 -0500

    Fixes #2, paket and sln (#3)
    * Fixes #2, paket and sln
    
    * Removing comment

--- src/FsCassy.Tests/InMemTests.fs
+++ src/FsCassy.Tests/InMemTests.fs
@@ -14,10 +14,34 @@ module Samples =
     open Statement
 
     type X = {x:int}
+    type T = {w:string; x:int; y:int; z:int}
+    type en = | Zero = 0 | One = 1
+    type E = { e:en }
+
     let exampleWhere : Statement<X,_> =
             table
         >>= where (Quote.X(fun x -> x.x = 0)) 
         >>= read
+
+    let exampleWhereNone : Statement<X,_> =
+            table
+        >>= where (Quote.X(fun x -> x.x = 10)) 
+        >>= read
+
+    let exampleFind : Statement<X,_> =
+            table
+        >>= where (Quote.X(fun x -> x.x = 0)) 
+        >>= find
+
+    let exampleFindConversion : Statement<E,_> =
+            table
+        >>= where (Quote.X(fun e -> int e.e = 0 )) 
+        >>= find
+
+    let exampleFindNone : Statement<X,_> =
+            table
+        >>= where (Quote.X(fun x -> x.x = 10)) 
+        >>= find
 
     let exampleTake : Statement<X,_> =
             table
@@ -32,7 +56,7 @@ module Samples =
     let exampleUpdate : Statement<X,_> =
             table
         >>= where (Quote.X(fun x -> x.x = 0))
-        >>= select (Quote.X(fun x -> { x with x = 10}))
+        >>= select (Quote.X(fun x -> { x with x = 10 }))
         >>= update
         >>= withConsistency Consistency.Any
         >>= execute
@@ -40,8 +64,16 @@ module Samples =
     let exampleUpdateIf : Statement<X,_> =
             table
         >>= where (Quote.X(fun x -> x.x = 0))
-        >>= select (Quote.X(fun x -> { x with x = 10}))
+        >>= select (Quote.X(fun x -> { x with x = 10 }))
         >>= updateIf (Quote.X(fun x -> x.x = 0))
+        >>= withConsistency Consistency.Any
+        >>= execute
+
+    let exampleUpdateNew : Statement<T,_> =
+            table
+        >>= where (Quote.X(fun t -> t.x = 7 && t.y = 8))
+        >>= select (Quote.X(fun t -> { t with z = 9 }))
+        >>= update
         >>= withConsistency Consistency.Any
         >>= execute
 
@@ -74,9 +106,30 @@ let ``InMem reads``() =
     |> Async.RunSynchronously |> List.ofSeq =! xs
 
 [<Test>]
-let ``InMem where``() = 
+let ``InMem reads where``() = 
     Samples.exampleWhere |> InMem.Interpreter.execute (ResizeArray xs) 
     |> Async.RunSynchronously |> List.ofSeq =! [{x=0}; {x=0}]
+
+[<Test>]
+let ``InMem reads no-where``() = 
+    Samples.exampleWhereNone |> InMem.Interpreter.execute (ResizeArray xs) 
+    |> Async.RunSynchronously |> List.ofSeq =! []
+
+[<Test>]
+let ``InMem finds where``() = 
+    Samples.exampleFind |> InMem.Interpreter.execute (ResizeArray xs) 
+    |> Async.RunSynchronously =! Some {x=0}
+
+[<Test>]
+let ``InMem finds no-where converted``() = 
+    let xs = ResizeArray [ { e = en.One } ]
+    Samples.exampleFindConversion |> InMem.Interpreter.execute (ResizeArray xs) 
+    |> Async.RunSynchronously =! None
+
+[<Test>]
+let ``InMem finds no-where``() = 
+    Samples.exampleFindNone |> InMem.Interpreter.execute (ResizeArray xs) 
+    |> Async.RunSynchronously =! None
 
 [<Test>]
 let ``InMem takes``() = 
@@ -108,6 +161,14 @@ let ``InMem updatesIf``() =
     Samples.exampleUpdateIf |> InMem.Interpreter.execute xs 
     |> Async.RunSynchronously
     xs.Contains {x=10} =! true
+
+[<Test>]
+let ``InMem updatesNew``() = 
+    let xs = ResizeArray [ { w = "0"; x=1; y=2; z=3 }; { w = "0"; x=2; y=3; z=4 }; { w = "0"; x=3; y=4; z=5 } ]
+    Samples.exampleUpdateNew |> InMem.Interpreter.execute xs 
+    |> Async.RunSynchronously
+    xs.Count =! 4
+    xs.Contains { w = null; x=7; y=8; z=9 } =! true
 
 [<Test>]
 let ``InMem deletes``() = 
